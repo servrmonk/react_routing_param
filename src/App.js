38a9267c7c745e85f0afa17f456da06839f47927
Raushan <roushankumarsingh2018@gmail.com>
2023-11-15 18:19:18 +0530

    finally details page start loading but same page for all id y...?

--- src/App.js
+++ src/App.js
@@ -35,8 +35,8 @@ function App() {
           />
           <Route path="/about" element={<About />} />
           <Route path="/signin" element={<SignIn />} />
-          {/* lekin is case me pura path exactly to match nai ho raa hai ye bs adha match ho raa hai /product adha part dynamic hai and adha part static */}
-          <Route path="/product" element={<Detail />} />
+          {/* lekin is case me pura path exactly to match nai ho raa hai ye bs adha match ho raa hai /product adha part dynamic hai and adha part static to dynamic part capture krne k lie yaha pr ek variable likh deta hai and us variable me variable me aane wala jo v dynamic path hai usko catch krte hai and wo variable url k path me hm likhte hai colons : k seperations k sath jaha pr colon aa jae path me : to uske baad jo v likha jaega wo variable consider hoga to main likh deta hoon id jaise ki yaha productId jo ki product/ k baad hai usko variable treat krega . ab jo v website pr product/ k baad aa raa hai jaise ki yaha pr id aa raa hai to id ko product id me copy kr k available kr dega */}
+          <Route path="/product/:productId" element={<Detail />} />
         </Routes>
         <Footer />
       </BrowserRouter>
